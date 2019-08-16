7e3b5436a5f1987f11b70d3f154f0f6fcd4d68fd
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-16 23:48:34 +0700

    add toggle favorite

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopapp/models/product.dart';
+import 'package:shopapp/providers/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
