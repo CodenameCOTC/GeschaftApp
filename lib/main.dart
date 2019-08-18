1bbcade3322a028351f99e50d3cab915825ce2ac
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-18 17:55:01 +0700

    add manage product

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shopapp/providers/orders.dart';
 
 import 'package:shopapp/providers/products.dart';
 import 'package:shopapp/screens/cart_screen.dart';
+import 'package:shopapp/screens/edit_product_screen.dart';
 import 'package:shopapp/screens/orders_screen.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
 import 'package:shopapp/screens/products_overview_screen.dart';
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
             UserProductScreen.routeName: (ctx) => UserProductScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
           }),
     );
   }
