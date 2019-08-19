03ced86e3066037353ee29ba0eccd5080d597e0f
A. Noval Adip <a.novaladip@yahoo.com>
2019-08-19 14:59:35 +0700

    add edit product on firebase

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -45,11 +45,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
 
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -69,13 +66,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
       }
     }
+    setState(() {
+      _isLoading = false;
+    });
+    Navigator.of(context).pop();
   }
 
   void _updateImageUrl() {
